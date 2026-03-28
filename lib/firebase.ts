cd578b8e2defec38d1af75dfd6c2096070386562
Senaya Bandara <senayabandara@Senayas-Air.bbrouter>
2026-03-28 14:17:50 +0530

    Fix page structure + clean hero/nav/products

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "torido-store.firebaseapp.com",
-  projectId: "torido-store",
-  storageBucket: "torido-store.appspot.com",
-  messagingSenderId: "515858093895",
-  appId: "1:515858093895:web:34ee07ee73f53a86a05e62",
-};
-
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+    apiKey: "AIzaSyAIxAbozlYw1Jhx-xmwO5odk0lrOcKo-yM",
+    authDomain: "torido-store.firebaseapp.com",
+    projectId: "torido-store",
+    storageBucket: "torido-store.firebasestorage.app",
+    messagingSenderId: "515858093895",
+    appId: "1:515858093895:web:34ee07ee73f53a86a05e62"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
