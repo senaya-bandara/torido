d7e15d8ea8ce80c536a7856dd57df4575a6520d5
senaya-bandara <159689093+senaya-bandara@users.noreply.github.com>
2026-05-31 20:10:35 +0530

    Create TshirtsHeroSlider.tsx

---
+++ app/components/TshirtsHeroSlider.tsx
@@ -0,0 +1,11 @@
+<section className="relative h-[500px] overflow-hidden">
+  <TshirtsHeroSlider />
+
+  <div className="absolute inset-0 bg-black/25" />
+
+  <div className="absolute inset-0 flex items-center">
+    <div className="max-w-7xl mx-auto px-6 w-full">
+      {/* Existing Hero Text */}
+    </div>
+  </div>
+</section>
