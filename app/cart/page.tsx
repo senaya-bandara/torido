873507baa2eee0d7843d77c3d56e34c4b43c595a
senaya-bandara <159689093+senaya-bandara@users.noreply.github.com>
2026-06-01 01:07:17 +0530

    Create page.tsx

---
+++ app/cart/page.tsx
@@ -0,0 +1,11 @@
+export default function CartPage() {
+  return (
+    <main className="max-w-7xl mx-auto px-6 py-24">
+      <h1 className="text-4xl font-bold mb-8">
+        Shopping Cart
+      </h1>
+
+      <p>Your cart is empty.</p>
+    </main>
+  );
+}
