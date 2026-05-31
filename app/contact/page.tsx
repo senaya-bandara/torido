00f1df055a234597f522b6d48827c439bd18ec17
senaya-bandara <159689093+senaya-bandara@users.noreply.github.com>
2026-05-31 21:03:22 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,16 @@
-
+import PageTransition from "@/app/components/PageTransition";
 export default function ContactPage() {
   return (
+
+      <PageTransition>
+
     <main className="max-w-7xl mx-auto px-6 py-24">
       <h1 className="text-4xl font-bold mb-6">Contact Us</h1>
 
       <p>Email: hello@torido.lk</p>
       <p>Phone: +94 77 123 4567</p>
     </main>
+      </PageTransition>
+
   );
 }
