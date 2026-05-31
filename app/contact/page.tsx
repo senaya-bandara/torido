41b631e16a5349abae0524e46a22767a7408d301
senaya-bandara <159689093+senaya-bandara@users.noreply.github.com>
2026-05-31 11:09:16 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,1 +1,11 @@
 
+export default function ContactPage() {
+  return (
+    <main className="max-w-7xl mx-auto px-6 py-24">
+      <h1 className="text-4xl font-bold mb-6">Contact Us</h1>
+
+      <p>Email: hello@torido.lk</p>
+      <p>Phone: +94 77 123 4567</p>
+    </main>
+  );
+}
